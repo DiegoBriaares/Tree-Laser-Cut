2a52d725d0be62441e33cfcbccc90a0aea2505c2
Diego González Hernández <diego.gonzalez@cimat.mx>
2018-12-04 22:58:27 -0600

    Draw code

--- draw_generation.py
+++ draw_generation.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Dec  2 18:30:18 2018
-
 @author: Diego González Hernández
 """
 
@@ -22,7 +21,8 @@ from dxfwrite import DXFEngine as dxf
 
 edges = []
 num_edges = 0
-num_points = 300
+num_points = 100
+plane_limit = 21
 name = 'tree.dxf'
 dwg = dxf.drawing(name)
 painted = [0 for i in range(0, num_points + 1)]
@@ -56,33 +56,33 @@ class DisjoinSet(object):
 
 def draw(a, b):
     if painted[a] == 0:
-        dwg.add(dxf.circle(center=(points_X[a],points_Y[a]), radius = .7, color = 141))
+        dwg.add(dxf.circle(center=(points_X[a],points_Y[a]), radius = .14, color = 141))
         painted[a] = 1
     if painted[b] == 0:
-        dwg.add(dxf.circle(center=(points_X[b],points_Y[b]), radius = .7, color = 141))
+        dwg.add(dxf.circle(center=(points_X[b],points_Y[b]), radius = .14, color = 141))
         painted[b] = 1
     r_1 = r_2 = r_3 = r_4 = 0
     if points_X[a] <= points_X[b]:
-        r_1 = 0.5
-        r_3 = -0.5
+        r_1 = 0.1
+        r_3 = -0.1
     else :
-        r_1 = -0.5
-        r_3 = 0.5
+        r_1 = -0.1
+        r_3 = 0.1
     if points_Y[a] <= points_Y[b]:
-        r_2 = 0.5
-        r_4 = -0.5
+        r_2 = 0.1
+        r_4 = -0.1
     else :
-        r_2 = -0.5
-        r_4 = 0.5
+        r_2 = -0.1
+        r_4 = 0.1
     dwg.add(dxf.line((points_X[a] + r_1, points_Y[a] + r_2), (points_X[b] + r_3, points_Y[b] + r_4), color=61))
 
 random.seed(a = None)
 for i in range(0, num_points):
-    points_X[i] = random.randint(1, 101)
-    points_Y[i] = random.randint(1, 101)
+    points_X[i] = random.randint(1, plane_limit)
+    points_Y[i] = random.randint(1, plane_limit)
     while used_points[points_X[i]][points_Y[i]] == 1:
-        points_X[i] = random.randint(1, 101)
-        points_Y[i] = random.randint(1, 101)
+        points_X[i] = random.randint(1, plane_limit)
+        points_Y[i] = random.randint(1, plane_limit)
     used_points[points_X[i]][points_Y[i]] = 1
 for i in range(0, num_points):
     for j in range(0, num_points):
